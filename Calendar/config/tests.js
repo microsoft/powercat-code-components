1fb6d21db207e16472b91634c2dddf822e0cf95f
scottdurow <scott_durow@hotmail.com>
2022-09-14 17:38:40 -0700

    fix:(Calendar) test failing due to dependency on today's date

--- Calendar/config/tests.js
+++ Calendar/config/tests.js
@@ -7,5 +7,8 @@ const Adapter = require('enzyme-adapter-react-16');
 // Initialize icons.
 initializeIcons(undefined, { disableWarnings: true });
 
+// Ensure the test snapshots are consistent when using today's date
+jest.useFakeTimers('modern').setSystemTime(new Date(2022, 8, 1, 12, 0, 0));
+
 // Configure enzyme.
 configure({ adapter: new Adapter() });
