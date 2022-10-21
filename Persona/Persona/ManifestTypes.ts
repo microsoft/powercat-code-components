89d53567a39388e670a3074a235b656b2ea1282c
Mehdi Slaoui Andaloussi <slaouist@hotmail.com>
2022-10-21 22:00:17 +0100

    Person Component

---
+++ Persona/Persona/ManifestTypes.ts
@@ -0,0 +1,23 @@
+import { PersonaPresence, PersonaSize } from '@fluentui/react';
+
+export const CanvasPersonaPresence = {
+    '0': PersonaPresence.none,
+    '1': PersonaPresence.offline,
+    '2': PersonaPresence.online,
+    '3': PersonaPresence.away,
+    '4': PersonaPresence.dnd,
+    '5': PersonaPresence.blocked,
+    '6': PersonaPresence.busy,
+};
+
+export const CanvasPersonaSizes = {
+    '17': PersonaSize.size8,
+    '10': PersonaSize.size24,
+    '11': PersonaSize.size32,
+    '12': PersonaSize.size40,
+    '13': PersonaSize.size48,
+    '16': PersonaSize.size56,
+    '14': PersonaSize.size72,
+    '15': PersonaSize.size100,
+    '18': PersonaSize.size120,
+};
---
+++ Persona/Persona/__mocks__/mock-parameters.ts
@@ -0,0 +1,21 @@
+/* istanbul ignore file */
+
+import { IInputs } from '../generated/ManifestTypes';
+import { MockEnumProperty, MockStringProperty, MockTwoOptionsProperty } from './mock-context';
+
+export function getMockParameters(): IInputs {
+    return {
+        AccessibilityLabel: new MockStringProperty(),
+        Theme: new MockStringProperty(),
+        PersonaSize: new MockEnumProperty(),
+        ImageUrl: new MockStringProperty(),
+        ImageAlt: new MockStringProperty(),
+        Presence: new MockEnumProperty(),
+        ImageInitials: new MockStringProperty(),
+        OptionalText: new MockStringProperty(),
+        HidePersonaDetails: new MockTwoOptionsProperty(),
+        TertiaryText: new MockStringProperty(),
+        Text: new MockStringProperty(),
+        SecondaryText: new MockStringProperty(),
+    };
+}
