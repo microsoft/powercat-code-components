5e8b9af901c8da9cea89bc15211f75ad6459aa24
PK-Microsoft <146996651+PK-Microsoft@users.noreply.github.com>
2024-10-04 15:51:57 -0700

    Update subwaynav-lifecycle.test.ts

--- SubwayNav/SubwayNav/_test_/subwaynav-lifecycle.test.ts
+++ SubwayNav/SubwayNav/_test_/subwaynav-lifecycle.test.ts
@@ -142,15 +142,6 @@ function createComponent() {
             [ItemColumns.Key]: 'Item 8',
             [ItemColumns.Label]: 'item8',
             [ItemColumns.State]: 'Custom',
-            [ItemColumns.ItemIcon]: 'AddFriend',
-            [ItemColumns.ItemColor]: '#C8A2C8',
-        }),
-        new MockEntityRecord('9', {
-            [ItemColumns.Key]: 'Item 9',
-            [ItemColumns.Label]: 'item8',
-            [ItemColumns.State]: 'Custom',
-            [ItemColumns.ItemIcon]: 'AddTo',
-            [ItemColumns.ItemColor]: '#EE82EE',
         }),
     ]);
     context.parameters.Theme.raw = JSON.stringify({
